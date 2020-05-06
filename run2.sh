74be570ec4834ab2f719ca15bd0d4aaf06bd0a53
Dmitry Rusakov (mcai-dr) <dmitry@magic-carpet.ch>
2020-05-06 12:38:51 +0000

    run2.sh: extended run.sh script for local runs added

---
+++ run2.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+###!/bin/bash
+
+# DR 05/05/2020: file modified for local runs
+#                environment variables taken from Dockerfile
+
+export DISPLAY=:0
+export TRADING_MODE=paper
+export TWS_PORT=4002
+export VNC_PORT=5900
+
+export IBGW_VERSION=972
+export IBC_CONFIG=/home/ubuntu/github/ib-gateway-docker/ibc_config.ini
+
+set -e
+set -o errexit
+
+rm -f /tmp/.X0-lock
+
+Xvfb :0 &
+sleep 1
+
+x11vnc -rfbport $VNC_PORT -display :0 -usepw -forever &
+socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
+
+# Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
+# Retry behavior can be implemented by re-running the container.
+/opt/ibc/scripts/ibcstart.sh "$IBGW_VERSION" --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD" "--ibc-ini=$IBC_CONFIG"
