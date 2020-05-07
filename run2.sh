e91a26ff3dcbe20248dd1d5cdef16a957b967f4a
Dmitry Rusakov <dmitry.rusakov@gmail.com>
2020-05-07 10:15:35 +0100

    run2.sh minor changes (3), for osx (wip)

--- run2.sh
+++ run2.sh
@@ -5,13 +5,21 @@
 #                environment variables taken from Dockerfile
 
 #export DISPLAY=:0
-export TRADING_MODE=paper
 export TWS_PORT=4002
 export VNC_PORT=5900
 
 export IBGW_VERSION=972
+export TRADING_MODE=paper
+
+export TWS_PATH=$HOME/Applications   # mac specific
+export TWS_SETTINGS=$HOME/Jts
+
+export IBC_PATH=/opt/ibc
 #export IBC_CONFIG=$HOME/github/ib-gateway-docker/ibc_config.ini
 export IBC_CONFIG=$HOME/mcai/github/ib-gateway-docker/ibc_config.ini
+
+# mac-specific
+export JAVA_PATH=`/usr/libexec/java_home`
 
 set -e
 set -o errexit
@@ -26,4 +34,7 @@ socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
 
 # Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
 # Retry behavior can be implemented by re-running the container.
-/opt/ibc/scripts/ibcstart.sh "$IBGW_VERSION" --gateway "--mode=$TRADING_MODE" "--user=$TWSUSERID" "--pw=$TWSPASSWORD" "--ibc-ini=$IBC_CONFIG"
+/opt/ibc/scripts/ibcstart.sh "$IBGW_VERSION" --gateway "--mode=$TRADING_MODE" \
+    "--tws-path=$TWS_PATH" "--tws-settings-path=$TWS_SETTINGS" \
+    "--ibc-path=$IBC_PATH" "--ibc-ini=$IBC_CONFIG" \
+    "--user=$TWSUSERID" "--pw=$TWSPASSWORD"
