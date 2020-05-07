2eab5a5ecd5aec845c9c1ac32eb786203ccef6b2
Dmitry Rusakov (mcai-lib) <dmitry@magic-carpet.ch>
2020-05-07 12:41:03 +0000

    run2.sh update and useful comments

--- run2.sh
+++ run2.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-###!/bin/bash
-
-# DR 05/05/2020: file modified for local runs
-#                environment variables taken from Dockerfile
-
-#export DISPLAY=:0
-export TWS_PORT=4002
-export VNC_PORT=5900
+# run.sh version for direct, non-docker runs
 
 export IBGW_VERSION=972
 export TRADING_MODE=paper
@@ -14,30 +7,34 @@ export TRADING_MODE=paper
 export TWS_SETTINGS=$HOME/Jts
 
 export IBC_PATH=/opt/ibc
-#export IBC_CONFIG=$HOME/github/ib-gateway-docker/ibc_config.ini
 export IBC_CONFIG=$HOME/mcai/github/ib-gateway-docker/ibc_config.ini
 
-# mac-specific
-if [[ $OSTYPE == darwin* ]]; then
-    export TWS_PATH=$HOME/Applications
-    export JAVA_PATH=`/usr/libexec/java_home`/bin
-fi
 
+# exit on error (both options are the same)
 set -e
 set -o errexit
 
+# enable virtual display either here or directly via 'xvfb-run <command>'
+export DISPLAY=:0
 rm -f /tmp/.X0-lock
-
 Xvfb :0 &
-sleep 1
 
-x11vnc -rfbport $VNC_PORT -display :0 -usepw -forever &
-socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
+# enable/disable remote control via x11vnc if needed
+# export VNC_PORT=5900
+# sleep 1
+# x11vnc -rfbport $VNC_PORT -display :0 -usepw -forever &
+
+# forking the outside port 4001 to internal 4002, no 'forever' option as in 'run.sh'
+# this is so that the IB GW will think that we are connecting from localhost
+socat TCP-LISTEN:4001,fork TCP:localhost:4002 &
 
 # echo all commands
 set -x
 
 if [[ $OSTYPE == darwin* ]]; then
+    export TWS_PATH=$HOME/Applications
+    export JAVA_PATH=`/usr/libexec/java_home`/bin
+
     # run latest, updated IBC scripts, deployed 3.2.2 version does not work for mac
     #
     # connection to IB GW on mac sporadic, sometimes works sometimes not,
@@ -53,8 +50,9 @@ if [[ $OSTYPE == darwin* ]]; then
         "--user=$TWSUSERID" "--pw=$TWSPASSWORD"
         # "--java-path=$JAVA_PATH"
 else
+    # non-mac, simple remote headless run
+
     /opt/ibc/scripts/ibcstart.sh "$IBGW_VERSION" --gateway "--mode=$TRADING_MODE" \
-        "--tws-settings-path=$TWS_SETTINGS" \
         "--ibc-path=$IBC_PATH" "--ibc-ini=$IBC_CONFIG" \
         "--user=$TWSUSERID" "--pw=$TWSPASSWORD"
 fi
